12ce39bc2e237117a240cdb4d20737ae411feb38
NESTOJEAN <NESTOJEAN>
2017-04-19 16:00:28 +0100

    Implemented the add function

--- app/complex.js
+++ app/complex.js
@@ -1,8 +1,44 @@
 'use strict'
-
+let util = require('util');
 module.exports = {
     addComplex: function(arg1,arg2){
-        
+        let argNo = arguments.length;
+        let rArr = 0;
+        let iArr = 0;
+        let sign='+';
+        for(let i=0; i< argNo;i++){
+            let cmplx = arguments[i];
+            let inputChr = cmplx.split('');
+            let real=true;
+            let img = false;
+            let tempR="";
+            let tempI="";
+            for(let j=0; j<inputChr.length; j++){
+                if(inputChr[j]==='-'||inputChr[j]==='+'&&j!=0)
+                {
+                    real=false;
+                    img=true;
+                    if(inputChr[j]==='-')
+                    tempI+=inputChr[j];
+                    continue;
+                }
+                if(real){
+                    tempR+=inputChr[j];
+                }else if(img){
+                    if(inputChr[j]==='i')
+                    continue;
+                    tempI+=inputChr[j]
+                }
+                
+            }
+            rArr += parseInt(tempR);
+            iArr += parseInt(tempI);
+           
+        }
+        if(iArr<1){
+            sign='';
+        }
+        return util.format('%s%s%si',rArr,sign, iArr);
     },
 
     subComplex: function(arg1,arg2){
@@ -19,10 +55,8 @@ module.exports = {
 
     transComplex: function(arg1,arg2){
 
-    },
-
-    parseComplex: function(arg){
-
     }
 
 };
+ let cmpx = require('./complex.js');
+console.log(cmpx.addComplex("2+3i","3+4i").isComplex);
--- spec/librarytest.js
+++ spec/librarytest.js
@@ -1,39 +1,111 @@
 
 var jasmine = require('jasmine');
-var expect = jasmine.expect;
-var complex = require('./app/complex.js')
+var complex = require('../app/complex.js')
 
-    describe("Complex number functions library", function(){
-        describe("adding two complex numbers", function(){
-            it("should add 2+3i to 3+4i", function(){
+    describe("Using the Complex number functions library: ", function(){
+        describe("when adding two complex numbers", function(){
+            it("should add 2+3i to 3+4i and return 5+7i", function(){
                 let result = complex.addComplex("2+3i","3+4i");
                 expect(result).toBe("5+7i");
+            });
+
+            it("should add 24+31i to 300+4i and return 324+35i", function(){
+                let result = complex.addComplex("24+31i","300+4i");
+                expect(result).toBe("324+35i");
+            });
+
+            it("should add 24+0i to 0+4i and return 24+4i", function(){
+                let result = complex.addComplex("24+0i","0+4i");
+                expect(result).toBe("24+4i");
+            });
+
+             it("should add correctly 34+4i, 0+4i, 2+41i", function(){
+                let result = complex.addComplex("34+4i","0+4i", "2+41i");
+                expect(result).toBe("36+49i");
+            });
+
+            it("should add correctly 8+4i, 20+6i, 2+4i", function(){
+                let result = complex.addComplex("8+4i","20+6i", "2+4i");
+                expect(result).toBe("30+14i");
             });
 
             it("should add 2-3i to 3+4i", function(){
                 let result = complex.addComplex("2-3i","3+4i");
                 expect(result).toBe("5+1i");
             });
+
+            it("should add 21-3i to 3+4i", function(){
+                let result = complex.addComplex("21-3i","3+4i");
+                expect(result).toBe("24+1i");
+            });
+
+            it("should add 21-3i, 3+4i, 3-12i", function(){
+                let result = complex.addComplex("21-3i","3+4i", "3-12i");
+                expect(result).toBe("27-11i");
+            });
+
+             it("should add 21-3i, 3+4i, 3-12i, 10+10i, 2-2i", function(){
+                let result = complex.addComplex("21-3i","3+4i", "3-12i","10+10i","2-2i");
+                expect(result).toBe("39-3i");
+            });
+
+            it("should add 21-3i to 30-4i", function(){
+                let result = complex.addComplex("21-3i","30-4i");
+                expect(result).toBe("51-7i");
+            });
+
+            it("should add -21-3i to 30-4i", function(){
+                let result = complex.addComplex("-21-3i","30-4i");
+                expect(result).toBe("9-7i");
+            });
             
         });
 
-        describe("subtracting complex numbers", function(){
+        describe("when subtracting complex numbers", function(){
 
         });
 
-        describe("multiplying complex numbers", function(){
+        describe("when multiplying complex numbers", function(){
 
         });
 
-        describe("dividing complex numbers", function(){
+        describe("when dividing complex numbers", function(){
 
         });
 
-        describe("transposing complex numbers", function(){
+        describe("when transposing complex numbers", function(){
 
         });
 
-        describe("parsing complex numbers", function(){
+        describe("when parsing complex numbers, ", function(){
+            // it("should return true when checking if 2+3i is a complex number", function(){
+            //     let num = complex.parseComplex("2+3i");
+            //     expect(num.isComplex).toBe(true);
+            // });
 
+            // it("should return true when checking if 50+31i is a complex number", function(){
+            //     let num = complex.parseComplex("50+31i");
+            //     expect(num.isComplex).toBe(true);
+            // });
+
+            // it("should return true when checking 3i+5 is a complex number", function(){
+            //     let num = complex.parseComplex("3i+5");
+            //     expect(num.isComplex).toBe(true);
+            // });
+
+            // it("should return false when checking -32+5i is a complex number", function(){
+            //     let num = complex.parseComplex("-32+5i");
+            //     expect(num.isComplex).toBe(true);
+            // });
+
+            // it("should return false when checking 3i+love is a complex number", function(){
+            //     let num = complex.parseComplex("3i+love");
+            //     expect(num.isComplex).toBe(false);
+            // });
+
+            // it("should return 3 as the imaginary part for 2+3i", function(){
+            //     let num = complex.parseComplex("2+3i");
+            //     expect(num.complex).toEqual(3);
+            // });
         });
     });
